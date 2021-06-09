ca324c76c81ab8ee7fa323e0f955dbed3a3673ba
nickyluo <nickyluo@tencent.com>
2021-06-09 23:40:02 +0800

    零钱兑换题解1:带备忘录的递归

---
+++ dp/322_1_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+//递归
+func coinChange(coins []int, amount int) int {
+	memo := make(map[int]int,amount)
+	return dp(coins,amount,&memo)
+}
+
+func dp (coins []int, amount int,memo *map[int]int) int {
+	if amount == 0 {
+		return 0
+	}
+	if amount < 0 {
+		return -1
+	}
+	if (*memo)[amount] != 0 {
+		return (*memo)[amount]
+	}
+	res := amount + 1
+	for _,coin := range coins {
+		//fmt.Println("coin:",coin)
+		subRes := dp(coins,amount-coin,memo)
+		if subRes == -1 {
+			continue
+		}
+		if subRes + 1 < res {
+			res = subRes + 1
+		}
+	}
+	if res == amount + 1 {
+		res = -1
+	}
+	(*memo)[amount] = res
+	//fmt.Println("amount:",amount)
+	//fmt.Println("res:",res)
+	//fmt.Println("memo:",*memo)
+	return res
+}
+
+
+func Test_322(t *testing.T) {
+	coins := []int{186,419,83,408}
+	fmt.Println(coinChange(coins,6248))
+}
